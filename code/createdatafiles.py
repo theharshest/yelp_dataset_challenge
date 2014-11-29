95dbd5fa6c573de1b96affab4e9fed89ad040bd2
John Maloney <jmmaloney3@gmail.com>
2014-11-29 08:58:06 -0600

    add code to create review_data.csv

--- code/createdatafiles.py
+++ code/createdatafiles.py
@@ -19,7 +19,7 @@ import datautils
 import sys
 
 def main():
-    if (len(sys.argv) < 7):
+    if (len(sys.argv) < 8):
         usage(sys.argv)
         return
 
@@ -29,20 +29,21 @@ def main():
     datafeats    = sys.argv[4]
     datacsv      = sys.argv[5]
     metacsv      = sys.argv[6]
+    revcsv       = sys.argv[7]
     
-    run_script(jsonbusfile, jsonrevfile, csvtractfile, datafeats, datacsv, metacsv)
+    run_script(jsonbusfile, jsonrevfile, csvtractfile, datafeats, datacsv, metacsv, revcsv)
 # end main
 
-def run_script(jsonbusfile, jsonrevfile, csvtractfile, datafeats, datacsv, metacsv):
+def run_script(jsonbusfile, jsonrevfile, csvtractfile, datafeats, datacsv, metacsv, revcsv):
     print('initializing data features')
     feat_info.init_data_feats(datafeats)
     
     print('creating data file and meta data file')
-    datautils.convert_restaurant_json_to_csv(jsonbusfile, jsonrevfile, csvtractfile, datacsv, metacsv)
+    datautils.convert_restaurant_json_to_csv(jsonbusfile, jsonrevfile, csvtractfile, datacsv, metacsv, revcsv)
 # end run_script
 
 def usage(argv):
-    print 'Usage: %s <jsonbusfile> <jsonrevfile> <csvtractfile> <datafeats> <datacsv> <metacsv>' % argv[0]
+    print 'Usage: %s <jsonbusfile> <jsonrevfile> <csvtractfile> <datafeats> <datacsv> <metacsv> <revcsv>' % argv[0]
 # end usage
 
 # run main method when this file is run from command line
